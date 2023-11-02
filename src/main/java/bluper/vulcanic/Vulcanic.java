39457e0bb415061d9b1b851aa47a9d7725015c6e
Bluperman949 <42910681+Blooperman949@users.noreply.github.com>
2023-11-02 16:14:16 -0700

    add faulty jei and remove examplemod code

---
+++ src/main/java/bluper/vulcanic/Vulcanic.java
@@ -0,0 +1,10 @@
+package bluper.vulcanic;
+
+@net.neoforged.fml.common.Mod(Vulcanic.MODID)
+public class Vulcanic {
+	public static final String MODID = "vulcanic";
+
+	public Vulcanic() {
+		
+	}
+}
